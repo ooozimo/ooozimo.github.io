a20e01b8f754958d4e4e4cde41e090a9d784f998
ooozimo <shamanbanan@outlook.com>
2022-02-10 16:53:35 +0100

    52252

--- src/puzzle-factory.js
+++ src/puzzle-factory.js
@@ -30,14 +30,14 @@ const COLORS = {
 
 // functions that return answers from PuzzleData class
 const QUESTIONS = {
-    'background color' : (d) => d.colors['background'],
-    'color text background color' : (d) => d.colors['colortext'],
-    'shape text background color' : (d) => d.colors['shapetext'],
-    'number color' : (d) => d.colors['number'],
-    'shape color' : (d) => d.colors['shape'],
-    'color text' : (d) => d.text[0],
-    'shape text' : (d) => d.text[1],
-    'shape' : (d) => d.shape
+    'bakgrunn farge' : (d) => d.colors['background'],
+    'farge text bakgrunn farge' : (d) => d.colors['colortext'],
+    'form text bakgrunn farge' : (d) => d.colors['shapetext'],
+    'nummer farge' : (d) => d.colors['number'],
+    'form farge' : (d) => d.colors['shape'],
+    'farge text' : (d) => d.text[0],
+    'form text' : (d) => d.text[1],
+    'form' : (d) => d.shape
 }
 
 class PuzzleData {
